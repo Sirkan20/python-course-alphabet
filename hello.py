9773572f40bb0f27ba257f342dbe01b21023c510
Sirkan20 <Sirkan20@gmail.com>
2020-05-01 00:22:35 +0300

    Modify file hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@ print("Hello Master")
 name = input('Print yourname here:  \n')
 name2 = list(map((lambda x: x*2), name))
 print(name2)
+name3 = ''.join(name2)
+print('Letss fun  {}'.format(name3))
 
 
 
